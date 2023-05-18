f98bf7c736d59567cccf5fc6ce897ac7fe7c42b2
next <next@qq.com>
2023-05-18 15:26:23 +0800

    v-5.6.0 测试版，新增lowjson数据库

--- server/generator/routes/aip.js
+++ server/generator/routes/aip.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 
@@ -23,6 +24,39 @@ router.get("/api/login", (request, response) =>
             message: "用户不存在",
             data: null
         })
+    }
+});
+
+// 获取昵称
+router.get("/api/name", (request, response) =>
+{
+
+    if (!request.query.name)
+    {
+        response.json({
+            code: 404,
+            data: null,
+            message: "没有你找的数据"
+        });
+    }
+
+    let user = db.get(key).find({ username: request.query.name }).value();
+
+    if (user)
+    {
+        response.json({
+            code: 200,
+            data: false,
+            message: "此昵称已经被注册"
+        });
+    }
+    else
+    {
+        response.json({
+            code: 200,
+            data: true,
+            message: "此昵称未注册"
+        });
     }
 })
 
